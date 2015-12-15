b068a4c09cb337391125b1424159d0314604671d
Jsoon <imwangxuesen@163.com>
2015-12-15 11:18:42 +0800

    改变工程名

---
+++ JsenProjectEncapsulation/JsenProjectEncapsulation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JsenProjectEncapsulation
+//
+//  Created by WangXuesen on 15/12/15.
+//  Copyright © 2015年 Jsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
